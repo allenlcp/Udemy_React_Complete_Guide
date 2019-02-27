9d3322ba52fa7cc571bf45abe3d01b6c6b5a2991
allenlcp <allen.decodemtl@gmail.com>
2019-02-27 17:08:11 -0500

    added section007

---
+++ section007_structuring_project/src/containers/App.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import classes from "./App.module.css";
+import Person from "../components/Persons/Person/Person";
+import Persons from "../components/Persons/Persons";
+import Cockpit from "../components/Cockpit/Cockpit";
+
+class App extends Component {
+  state = {
+    persons: [
+      { id: "asd", name: "Max", age: 28 },
+      { id: "daf", name: "Manu", age: 29 },
+      { id: "gfg", name: "Stephanie", age: 26 }
+    ],
+    otherState: "some other value",
+    showPersons: false
+  };
+
+  nameChangeHandler = (event, id) => {
+    // could have used indexOf() here
+    // I'm finding the index of the person to be updated
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    // another way - old
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+
+    // getting copy of objects
+    // get a copy of the person object
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+    // change name of the person object
+    person.name = event.target.value;
+
+    // get a copy of the persons array
+    const persons = [...this.state.persons];
+    // update the person object from the persons array to the updated one
+    persons[personIndex] = person;
+
+    // updating old persons array with the new copy
+    this.setState({ persons: persons });
+  };
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+
+  deletePersonHandler = personIndex => {
+    const persons = this.state.persons.slice();
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
+
+  render() {
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = 
+        <Persons
+          persons={this.state.persons}
+          click={this.deletePersonHandler}
+          changed={this.nameChangeHandler}/>;
+    }
+
+    return (
+      <div className={classes.App}>
+        <Cockpit
+          title={this.props.appTitle}
+          persons={this.state.persons}
+          showPersons={this.state.showPersons}
+          clicked={this.togglePersonsHandler}
+        />
+        {persons}
+      </div>
+    );
+  }
+}
+
+export default App;
