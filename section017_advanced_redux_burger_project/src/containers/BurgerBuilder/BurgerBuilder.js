9fc15a888d57f66badbb8bb418566dbdb1cf08f1
allenlcp <allen.decodemtl@gmail.com>
2019-03-25 17:20:12 -0400

    Restructured folder using actionTypes

--- section017_advanced_redux_burger_project/src/containers/BurgerBuilder/BurgerBuilder.js
+++ section017_advanced_redux_burger_project/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,7 +9,7 @@ import Aux from "../../hoc/Aux/Aux";
 import axios from "../../axios-orders";
 import Spinner from "../../components/UI/spinner/Spinner";
 import withErrorHandler from "../../hoc/WithErrorHandler/withErrorHandler";
-import * as actionTypes from "../../store/actions";
+import * as burgerBuilderActions from "../../store/actions/index";
 
 class BurgerBuilder extends Component {
   state = {
@@ -116,8 +116,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onIngredientAdded: (ingName) => dispatch({type: actionTypes.ADD_INGREDIENT, ingredientName: ingName}),
-    onIngredientRemoved: (ingName) => dispatch({type: actionTypes.REMOVE_INGREDIENT, ingredientName: ingName}),
+    onIngredientAdded: (ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)),
+    onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName)),
   }
 }
 
